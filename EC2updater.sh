d80b97d304c866cc5add37507aaeac110739f078
GK <georgek@gmail.com>
2014-02-22 15:28:40 -0500

    Chapter 10

--- EC2updater.sh
+++ EC2updater.sh
@@ -2,8 +2,17 @@
 # updates ec2 instance
 
 project='django_test'
+
+# fetch repo updates
 git init
 git remote add origin https://github.com/ugik/$project.git
 git fetch --all
 git reset --hard origin/master
+
+# update database
+cd $project
+python manage.py syncdb
+
+# restart Apache
+sudo service apache2 restart
 
