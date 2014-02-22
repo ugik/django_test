efdffc1c4bab593071e9c7287eb2cf8b90c94552
GK <georgek@gmail.com>
2014-02-22 14:28:35 -0500

    Chapter 9

--- article/urls.py~
+++ article/urls.py~
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
 
 	url(r'^all/$', 'article.views.articles'),
 	url(r'^get/(?P<article_id>\d+)/$', 'article.views.article'),
+	url(r'^language/(?P<language>[a-z\-]+)/$', 'article.views.language'),
 
 )
--- django_test/urls.py
+++ django_test/urls.py
@@ -7,8 +7,17 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
 	(r'^articles/', include('article.urls')),
-
+# admin
     url(r'^admin/', include(admin.site.urls)),
+
+# user auth
+	url(r'^accounts/login/$', 'django_test.views.login'),
+	url(r'^accounts/auth/$', 'django_test.views.auth_view'),
+	url(r'^accounts/logout/$', 'django_test.views.logout'),
+	url(r'^accounts/loggedin/$', 'django_test.views.loggedin'),
+	url(r'^accounts/invalid/$', 'django_test.views.invalid_login'),
+
+# Hello examples
 	url(r'hello/$', 'article.views.hello'),
 	url(r'hello_template/$', 'article.views.hello_template'),
 	url(r'hello_template_simple/$', 'article.views.hello_template_simple'),
