656652583b005e45b5312ecae92da9b657d399d8
GK <georgek@gmail.com>
2014-02-22 15:16:58 -0500

    Chapter 10

--- django_test/urls.py
+++ django_test/urls.py
@@ -10,12 +10,14 @@ urlpatterns = patterns('',
 # admin
     url(r'^admin/', include(admin.site.urls)),
 
-# user auth
+# user auth & registration
 	url(r'^accounts/login/$', 'django_test.views.login'),
 	url(r'^accounts/auth/$', 'django_test.views.auth_view'),
 	url(r'^accounts/logout/$', 'django_test.views.logout'),
 	url(r'^accounts/loggedin/$', 'django_test.views.loggedin'),
 	url(r'^accounts/invalid/$', 'django_test.views.invalid_login'),
+	url(r'^accounts/register/$', 'django_test.views.register_user'),
+	url(r'^accounts/register_success/$', 'django_test.views.register_success'),
 
 # Hello examples
 	url(r'hello/$', 'article.views.hello'),
