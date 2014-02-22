8bf4dbd3131865e6ac7c1cb433ee651984c70c8c
GK <georgek@gmail.com>
2014-02-22 10:47:15 -0500

    new bash scripts

--- EC2update.sh
+++ EC2update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "updating EC2 instance..."
 
-ssh -i ~/Downloads/ec2.pem ubuntu@"$1" bash updater.sh
+ssh -i ~/Downloads/ec2.pem ubuntu@"$1" bash EC2updater.sh
 
 # eg.
 # ssh -i ~/Downloads/ec2.pem ubuntu@ec2-54-213-195-214.us-west-2.compute.amazonaws.com
