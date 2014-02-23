3f9baad7f7b237bd8e778a58daff1dca23afd6d6
GK <georgek@gmail.com>
2014-02-22 23:18:43 -0500

    test

--- EC2update.sh
+++ EC2update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "updating EC2 instance..."
 
-ssh -i ~/Downloads/ec2.pem ubuntu@"$1" bash EC2updater.sh
+ssh-agent bash -c 'ssh-add ~/Downloads/ec2.pem; git push django_test.com +master:refs/heads/master'
 
 # eg.
 # ssh -i ~/Downloads/ec2.pem ubuntu@ec2-54-213-195-214.us-west-2.compute.amazonaws.com
