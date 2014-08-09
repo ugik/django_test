0b4f07b0ba7250a9e8409a873d8cf9df75ea5b5a
GK <georgek@gmail.com>
2014-08-09 16:00:37 -0400

    checkpoint

--- article/views.py
+++ article/views.py
@@ -54,21 +54,21 @@ def language(request, language='en-us'):
     return response
 
 def create(request):
+    
+    form = ArticleForm()
+
     if request.POST:
         form = ArticleForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-
             return HttpResponseRedirect('/articles/all')
-	else:
-		form = ArticleForm()
 
-	args = {}
-	args.update(csrf(request))
+    args = {}
+    args.update(csrf(request))
 
-	args['form'] = form
+    args['form'] = form
 
-	return render_to_response('create_article.html', args)
+    return render_to_response('create_article.html', args, context_instance=RequestContext(request))
 
 def like_article(request, article_id):
 	if article_id:
